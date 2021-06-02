3f8bb1e04dbc9063176983c7ab91d99b3e437c33
Jin Haneul <knsan189@naver.com>
2021-06-02 20:24:36 +0900

    댓글 input 실시간 감지 추가

--- js/style.js
+++ js/style.js
@@ -1,4 +1,19 @@
 window.addEventListener('DOMContentLoaded', function () {
+    
+    const newComment = document.getElementById('new-comment')
+    const commitSubmit = document.getElementById('comment-submit')
+    
+    const buttonChange = (event) => {
+        if(event.currentTarget.value) commitSubmit.classList.add('active')
+        else commitSubmit.classList.remove('active')
+    }
+
+    newComment.addEventListener('change', buttonChange)
+    newComment.addEventListener('propertychange', buttonChange)
+    newComment.addEventListener('keyup', buttonChange)
+    newComment.addEventListener('paste', buttonChange)
+
+
 
     // 댓글 삭제 수정 버튼 토글기능
     const moreBtn = document.querySelectorAll('.more-btn')
@@ -20,4 +35,5 @@ window.addEventListener('DOMContentLoaded', function () {
         event.stopPropagation()
     })
     
+
 })
