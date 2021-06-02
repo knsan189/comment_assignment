57a7d62107fa2a89070a648590310253fe1cfb2d
Jin Haneul <knsan189@naver.com>
2021-06-02 20:39:26 +0900

    댓글 추가 기능 추가

--- js/comment.js
+++ js/comment.js
@@ -1,3 +1,31 @@
 window.addEventListener('DOMContentLoaded', function(){
-    
+
+    const isEmtpy = () => {
+        alert('내용을 입력해주세요')
+    }
+
+    const newCommentInput = document.getElementById('new-comment')
+    const submitComment = document.getElementById('comment-submit')
+
+    const onSubmit = () => {
+
+        const newComment = newCommentInput.value.trim()
+
+        if(newComment){
+            const content = document.createElement('div')
+            content.classList.add('comment-content')
+            content.textContent = newComment
+
+            const comment = document.createElement('div')
+            comment.classList.add('comment-row')
+            comment.appendChild(content)
+
+            document.querySelector('.comment-list').appendChild(comment)
+        }
+    }
+
+    submitComment.addEventListener('click', onSubmit)
+    newCommentInput.addEventListener('keypress', function(event) {
+        event.key === 'Enter' && onSubmit
+    })
 })
