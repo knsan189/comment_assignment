a5847e8c2210846b1bb5b2feb6b54262f17cbc23
Jin Haneul <knsan189@naver.com>
2021-06-03 20:38:33 +0900

    작성 날짜 구하는 방식 수정

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 프론트엔드 진하늘 과제
 
-https://knsan189.github.io/comment_assignment/
+사이트 주소 : https://knsan189.github.io/comment_assignment/
--- js/comment.js
+++ js/comment.js
@@ -51,10 +51,26 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // 작성일자 구하는 함수
     const dateToString = date => {
-        const dateString = date.toISOString()
-        const dateToString = dateString.substring(0, 10) + " " + dateString.substring(11, 19)
 
-        return dateToString
+        const year = date.getFullYear().toString();
+
+        let month = date.getMonth() + 1;
+        month = month < 10 ? '0' + month.toString() : month.toString();
+
+        let day = date.getDate();
+        day = day < 10 ? '0' + day.toString() : day.toString();
+
+        let hour = date.getHours();
+        hour = hour < 10 ? '0' + hour.toString() : hour.toString();
+
+        let minites = date.getMinutes();
+        minites = minites < 10 ? '0' + minites.toString() : minites.toString();
+
+        let seconds = date.getSeconds();
+        seconds = seconds < 10 ? '0' + seconds.toString() : seconds.toString();
+
+        return `${year}-${month}-${day} ${hour}:${minites}:${seconds}`;
+
     }
 
     const deleleFunction = () => {
